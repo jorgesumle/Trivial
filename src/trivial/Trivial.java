32f0561a1eb12e5acbf8e17758bd662a93f3cede
DAW15 <DAW15@PC15>
2016-02-09 11:51:02 +0100

    He creado el menú del modo de edición y la clase para la entrada de datos.

--- src/trivial/Trivial.java
+++ src/trivial/Trivial.java
@@ -7,7 +7,6 @@ package trivial;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
