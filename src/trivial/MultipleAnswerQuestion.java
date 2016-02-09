80a0c5cc3ab90cea9f6d57c87fb53f4138960e07
Jorge <jorgesumle@gmail.com>
2016-02-09 23:04:42 +0100

    He terminado de crear todas las clases del proyecto y muchos de sus métodos. Ya he terminado el esqueleto básico del proyecto.

---
+++ src/trivial/MultipleAnswerQuestion.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2016 Jorge Maldonado Ventura 
+ *
+ * Este programa es software libre: usted puede redistruirlo y/o modificarlo
+ * bajo los términos de la Licencia Pública General GNU, tal y como está publicada por
+ * la Free Software Foundation; ya sea la versión 2 de la Licencia, o
+ * (a su elección) cualquier versión posterior.
+ *
+ * Este programa se distribuye con la intención de ser útil,
+ * pero SIN NINGUNA GARANTÍA; incluso sin la garantía implícita de
+ * USABILIDAD O UTILIDAD PARA UN FIN PARTICULAR. Vea la
+ * Licencia Pública General GNU para más detalles.
+ *
+ * Usted debería haber recibido una copia de la Licencia Pública General GNU
+ * junto a este programa.  Si no es así, vea <http://www.gnu.org/licenses/>.
+ */
+
+package trivial;
+
+import java.util.List;
+
+/**
+ *
+ * @author Jorge Maldonado Ventura 
+ */
+public class MultipleAnswerQuestion implements Answer{
+    @Override
+    public List check(int code) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
