827168479e7b4caee5aee3a2d5e395e5d6684c7b
Jorge <jorgesumle@gmail.com>
2016-03-05 09:54:33 +0100

    He escrito algo de javadoc y he mejorado la apariencia y la funcionalidad del programa.

--- src/Console/Input.java
+++ src/Console/Input.java
@@ -21,10 +21,15 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 /**
- *
+ * Se encarga de las operaciones de entrada de datos por teclado.
  * @author Jorge Maldonado Ventura 
  */
 public class Input {
+    /**
+     * Introduce una respuesta por teclado. Las respuestas no pueden tener más de 30 caracteres.
+     * @param message el mensaje que aparece al preguntar por la respuesta.
+     * @return la respuesta introducida.
+     */
     public static String answerInput(String message){
         String answer;
         do{
@@ -37,6 +42,11 @@ public class Input {
         } while(answer.length() > Answer.ANSWER_LENGTH || StringFormat.removeSpacesAtTheBeggining(answer).length() < 1);
         return answer;
     }
+    /**
+     * Introduce un número entre -128 y 127 por teclado.
+     * @param message el mensaje que aparece al preguntar por el número.
+     * @return el número introducido.
+     */
     public static byte byteInput(String message){
         System.out.print(message);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -52,6 +62,7 @@ public class Input {
         }
         return value;
     }
+    
     public static String input(String message){
         System.out.print(message);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -67,6 +78,11 @@ public class Input {
         }
         return value;
     }
+    /**
+     * Introduce un número entre -2147483648 y 2147483647 por teclado.
+     * @param message el mensaje que aparece al preguntar por el número.
+     * @return el número introducido.
+     */
     public static int intInput(String message){
         System.out.print(message);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -94,6 +110,12 @@ public class Input {
         } while(question.length() > Question.QUESTION_LENGTH || StringFormat.removeSpacesAtTheBeggining(question).length() < 1);
         return question;
     }
+    /**
+     * Elige una de las seis categorías disponibles por teclado. Las categorías son las siguientes: 1) Geografía,
+     * 2) Cine y espectáculos, 3) Historia, 4) Arte y literatura, 5) Ciencias y naturaleza y 6) Deportes y
+     * pasatiempos.
+     * @return el número de la categoría elegida.
+     */
     public static byte selectCategory() {
         byte option = 0;
         final String categories = "    1) Geografía.\n"
